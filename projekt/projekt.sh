08f57ccdfce7d225030cfcb30c2538be7274836a
Richard <richard.staszkiewicz@gmail.com>
2023-03-01 08:18:17 +0100

    [Projekt] Main switch beginning implementation

--- projekt/projekt.sh
+++ projekt/projekt.sh
@@ -10,14 +10,29 @@ Usage: projekt.sh [OPTION]... DIRECTORIES...
     -c  --copy          Copy files to directory X
     -d  --duplicates    Remove duplicates
     -e  --empty         Remove empty files
-    -t  --temporary     Remove temporary
+    -t  --temporary     Remove temporary files
     -n  --namesake      Preserve the newest of namesake files
     -p  --permissions   Set permissions to default
     -s  --symbols       Substitute problematic symbols with pre-chosen ('.')
     -r  --rename        Enable hot-plugged file rename
+    -f  --fastforward   Do not interact with caller: use default choices
 EOF
     exit 0;
 }
+
+
+while getopts ":h" option; do
+    case $option in
+        h)
+            exe_help
+            exit
+            ;;
+        \?)
+            echo Something
+            exit 1
+            ;;
+    esac
+done
 
 
 
